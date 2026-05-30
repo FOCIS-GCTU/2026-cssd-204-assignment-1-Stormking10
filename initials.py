b45ae670fec5815b5c0268d21a06aaabcb297f54
stormking10 <bentiljunior10@gmail.com>
2026-05-30 13:44:10 +0000

    Update initials.py with assignment solution

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Arabogo Bentil Awinzalit Junior
+# STUDENT ID: 2425403359
+# Email: 2425403359@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Arabogo Bentil Awinzalit Junior, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in normal and large stylized letters.
 
+    print()
+    print("...ABAJ")
+    print()
+
+    print("...AAAAAAAAAAAA........ BBBBBBBBBBBB............AAAAAAAAAAAA........JJJJJJJJJJJJ...****")
+    print("...AAAAAAA AAAAA........BBBBBBBBBBBB ...........AAAAAAA AAAAA........JJJJJJJJJJJJ...****")
+    print("...AAAAA   AAAAA........BBBBB    BBB...........AAAAA   AAAAA........    JJJJ    ...****")
+    print("...AAAA     AAAA........BBBBB    BBB...........AAAA     AAAA........    JJJJ    ...****")
+    print("...AAAAAAAAAAAA.........BBBBBBBBBBB ...........AAAAAAAAAAAA.........    JJJJ    ...****")
+    print("...AAAAAAAAAAAA.........BBBBBBBBBBB ...........AAAAAAAAAAAA.........    JJJJ    ...****")
+    print("...AAAA     AAAA........BBBBB    BBB...........AAAA     AAAA........    JJJJ    ...****")
+    print("...AAAA     AAAA........BBBBB    BBB...........AAAA     AAAA........JJJJJJJJ    ...****")
+    print("...AAAA     AAAA........BBBBBBBBBBB ...........AAAA     AAAA........JJJJJJJJ    ...****")
+    print("...AAAA     AAAA........BBBBBBBBBBBB...........AAAA     AAAA........JJJJJJJJJ   ...****")
+
+    print()
 
 
 main()
+
+
